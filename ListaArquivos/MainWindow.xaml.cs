a10e663e5015c22984bd13e52b9227b0a04849de
Lucas César <lucas.cesar.8@hotmail.com>
2017-10-28 00:32:01 -0200

    Added support to read zip files

--- ListaArquivos/MainWindow.xaml.cs
+++ ListaArquivos/MainWindow.xaml.cs
@@ -3,12 +3,15 @@ using System.Text;
 using System.Windows;
 using System.Windows.Forms;
 using System.Windows.Threading;
+using System.IO.Compression;
+//using System.IO;
 
-namespace ListaArquivos
-{
+namespace ListaArquivos {
 	public partial class MainWindow : Window {
 
 		private String sPath;
+		private String fn;
+		private String ext;
 		private Int64 total;
 		private Delimon.Win32.IO.DirectoryInfo folder;
 		private String baseDir = AppDomain.CurrentDomain.BaseDirectory;
@@ -69,13 +72,43 @@ namespace ListaArquivos
 
 				foreach (Delimon.Win32.IO.FileInfo f in folder.GetFiles()) {
 					sb.Append($"{Environment.NewLine}{f.Length}\t{f.DirectoryName}\t{f.Name}\t{f.Extension}");
-					progressBar.Dispatcher.Invoke(() => ++progressBar.Value, DispatcherPriority.Background);
+					progressBar.Dispatcher.Invoke(() => progressBar.Value++, DispatcherPriority.Background);
+
+					if (f.Extension.Equals(".zip", StringComparison.OrdinalIgnoreCase)) {
+						using(ZipArchive archive = ZipFile.OpenRead(f.FullName)) {
+
+							foreach(ZipArchiveEntry entry in archive.Entries) {
+								ext = entry.Name.Substring(entry.Name.LastIndexOf("."));
+								sb.Append($"{Environment.NewLine}{entry.Length}\t{f.FullName}\t{entry.Name}\t{ext}");
+
+								if(ext.Equals(".zip", StringComparison.OrdinalIgnoreCase)) {
+									fn = f.FullName;
+									ZipList(fn, entry);
+								}
+							}
+						}
+					}
 				}
 
 				foreach (string d in Delimon.Win32.IO.Directory.GetDirectories(sDir)) {
 					CreateList(d);
 				}
-			} catch {  }
+			} catch { }
+		}
+
+		private void ZipList(string fullPath, ZipArchiveEntry entry) {
+			using(ZipArchive a = new ZipArchive(entry.Open())) {
+
+				foreach(ZipArchiveEntry e in a.Entries) {
+					ext = e.Name.Substring(e.Name.LastIndexOf("."));
+					sb.Append($"{Environment.NewLine}{e.Length}\t{fullPath}\\{entry.FullName}\t{e.Name}\t{ext}");
+
+					if(ext.Equals(".zip", StringComparison.OrdinalIgnoreCase)) {
+						fn = $@"{fullPath}\{entry.FullName}";
+						ZipList(fn, e);
+					}
+				}
+			}
 		}
 
 		private void create_List(object sender, RoutedEventArgs e) {
@@ -85,12 +118,15 @@ namespace ListaArquivos
 
 			//Stopwatch stopWatch = new Stopwatch();
 			//stopWatch.Start();
+
 			CreateList(sPath);
 			System.IO.File.WriteAllText(baseDir + @"lista_arquivos.txt", sb.ToString());
+
 			//stopWatch.Stop();
 			//TimeSpan ts = stopWatch.Elapsed;
 			//string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}", ts.Hours, ts.Minutes, ts.Seconds, ts.Milliseconds / 10);
 			//Console.WriteLine("RunTime " + elapsedTime);
+
 			button.IsEnabled = true;
 			button1.IsEnabled = true;
 			System.Windows.Forms.MessageBox.Show($"O arquivo 'lista_arquivos.txt' foi criado em{Environment.NewLine}{baseDir}", "Arquivos processados com sucesso!", MessageBoxButtons.OK, MessageBoxIcon.Information);
