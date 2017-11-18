8615e06303cd31ce0a39f5107c939b30491379dd
Lucas César <lucas.cesar.8@hotmail.com>
2017-11-17 23:07:17 -0200

    Improved memory performance to prevent crashes in really large files

--- ListaArquivos/MainWindow.xaml.cs
+++ ListaArquivos/MainWindow.xaml.cs
@@ -6,6 +6,7 @@ using System.Windows.Threading;
 using System.IO.Compression;
 using System.Windows.Input;
 using System.Diagnostics;
+using System.Collections.Generic;
 
 namespace ListaArquivos {
 	public partial class MainWindow : Window {
@@ -17,14 +18,15 @@ namespace ListaArquivos {
 		private Delimon.Win32.IO.DirectoryInfo folder;
 		private String baseDir = AppDomain.CurrentDomain.BaseDirectory;
 		private Boolean validDir = true;
-		private StringBuilder sb;
+		//private StringBuilder sb;
 		//private StringBuilder zip;
 		private StringBuilder rep;
 		private System.IO.FileStream fs;
 		private Version version = System.Reflection.Assembly.GetExecutingAssembly().GetName().Version;
-		//private String name;
+        //private String name;
+        private List<String> lista = new List<String>();
 
-		public MainWindow() {
+        public MainWindow() {
 			InitializeComponent();
 			//String stringVersion = System.Reflection.Assembly.GetExecutingAssembly().GetName().Version.ToString();
 			Title = $"i-luminas - Lista Arquivos {version.Major}.{version.Minor}.{version.Build.ToString().Substring(0,1)}";
@@ -107,11 +109,27 @@ namespace ListaArquivos {
 			try {
 				folder = new Delimon.Win32.IO.DirectoryInfo(sDir);
 
-				foreach (Delimon.Win32.IO.FileInfo f in folder.GetFiles()) {
-					sb.Append($"{Environment.NewLine}{f.Length}\t{f.DirectoryName}\t{f.Name}\t{f.Extension.ToLower()}");
-					progressBar.Dispatcher.Invoke(() => progressBar.Value++, DispatcherPriority.Background);
+				foreach(Delimon.Win32.IO.FileInfo f in folder.GetFiles()) {
+                    
+                    lista.Add($"{f.Length}\t{f.DirectoryName}\t{f.Name}\t{f.Extension.ToLower()}");
+                    //await FileWriteAsync($"{Environment.NewLine}{f.Length}\t{f.DirectoryName}\t{f.Name}\t{f.Extension.ToLower()}");
+                    //using (System.IO.StreamWriter w = Delimon.Win32.IO.File.AppendText($"{baseDir}lista_arquivos.txt")) {
+                    //    w.Write($"{Environment.NewLine}{f.Length}\t{f.DirectoryName}\t{f.Name}\t{f.Extension.ToLower()}");
+                    //    w.Close();
+                    //    w.Dispose();
+                    //}
+                    //sb.Append($"{Environment.NewLine}{f.Length}\t{f.DirectoryName}\t{f.Name}\t{f.Extension.ToLower()}");
+                    if(lista.Count >= 100000) {
+                        System.IO.File.AppendAllLines($"{baseDir}lista_arquivos.txt", lista);
+                        //List<List<String>> l = new List<List<String>>();
+                        //l.Add(lista.ToList());
+                        lista.Clear();
+                        lista.TrimExcess();
+                    }
 
-					if (f.Extension.Equals(".zip", StringComparison.OrdinalIgnoreCase)) {
+                    progressBar.Dispatcher.Invoke(() => progressBar.Value++, DispatcherPriority.Background);
+
+					if(f.Extension.Equals(".zip", StringComparison.OrdinalIgnoreCase)) {
 						//using (var fs = new System.IO.StreamReader(Delimon.Win32.IO.File.OpenRead(f.FullName), Encoding.GetEncoding(850))) {
 						//using(ZipArchive archive = new ZipArchive(fs, ZipArchiveMode.Read, Encoding.GetEncoding(850))) {
 
@@ -138,9 +156,24 @@ namespace ListaArquivos {
 									} else {
 										rep.Clear();
 									}
-									sb.Append($"{Environment.NewLine}{entry.Length}\t{f.FullName}{rep}\t{entry.Name}\t{ext}");
 
-									if(ext.Equals(".zip", StringComparison.OrdinalIgnoreCase)) {
+
+                                    lista.Add($"{entry.Length}\t{f.FullName}{rep}\t{entry.Name}\t{ext}");
+                                    //await FileWriteAsync($"{Environment.NewLine}{entry.Length}\t{f.FullName}{rep}\t{entry.Name}\t{ext}");
+                                    //using (System.IO.StreamWriter w = Delimon.Win32.IO.File.AppendText($"{baseDir}lista_arquivos.txt")) {
+                                    //    w.Write($"{Environment.NewLine}{entry.Length}\t{f.FullName}{rep}\t{entry.Name}\t{ext}");
+                                    //    w.Close();
+                                    //    w.Dispose();
+                                    //}
+                                    //sb.Append($"{Environment.NewLine}{entry.Length}\t{f.FullName}{rep}\t{entry.Name}\t{ext}");
+
+                                    if(lista.Count >= 100000) {
+                                        System.IO.File.AppendAllLines($"{baseDir}lista_arquivos.txt", lista);
+                                        lista.Clear();
+                                        lista.TrimExcess();
+                                    }
+
+                                if (ext.Equals(".zip", StringComparison.OrdinalIgnoreCase)) {
 										fn = f.FullName;
 										ZipList(fn, entry);
 									}
@@ -182,9 +215,23 @@ namespace ListaArquivos {
 							rep.Append($"{e.FullName}");
 							rep.Replace("/", @"\");
 						}
-						sb.Append($"{Environment.NewLine}{entry.Length}\t{fullPath}{rep}\t{entry.Name}\t{ext}");
+                        
+                        lista.Add($"{entry.Length}\t{fullPath}{rep}\t{entry.Name}\t{ext}");
+                        //await FileWriteAsync($"{Environment.NewLine}{entry.Length}\t{fullPath}{rep}\t{entry.Name}\t{ext}");
+                        //using(System.IO.StreamWriter w = Delimon.Win32.IO.File.AppendText($"{baseDir}lista_arquivos.txt")) {
+                        //    w.Write($"{Environment.NewLine}{entry.Length}\t{fullPath}{rep}\t{entry.Name}\t{ext}");
+                        //    w.Close();
+                        //    w.Dispose();
+                        //}
+                        //sb.Append($"{Environment.NewLine}{entry.Length}\t{fullPath}{rep}\t{entry.Name}\t{ext}");
 
-						if(ext.Equals(".zip", StringComparison.OrdinalIgnoreCase)) {
+                        if(lista.Count >= 100000) {
+                            System.IO.File.AppendAllLines($"{baseDir}lista_arquivos.txt", lista);
+                            lista.Clear();
+                            lista.TrimExcess();
+                        }
+
+                        if(ext.Equals(".zip", StringComparison.OrdinalIgnoreCase)) {
 							fn = $@"{fullPath}{rep}";
 							ZipList(fn, entry);
 						}
@@ -193,14 +240,25 @@ namespace ListaArquivos {
 			}
 		}
 
-		private void create_List(object sender, RoutedEventArgs e) {
+        //async public Task FileWriteAsync(string line) {
+
+        //    using(System.IO.StreamWriter w = Delimon.Win32.IO.File.AppendText($"{baseDir}lista_arquivos.txt")) {
+        //        await w.WriteAsync(line);
+        //    }
+        //}
+
+        private void create_List(object sender, RoutedEventArgs e) {
+
+            if(System.IO.File.Exists($"{baseDir}lista_arquivos.txt")) System.IO.File.Delete($"{baseDir}lista_arquivos.txt");
 			progressBar.Value = 0;
 			button.IsEnabled = false;
 			button1.IsEnabled = false;
-			sb = new StringBuilder("Tamanho (bytes)\tCaminho\tArquivo\tExtensão");
-			//zip = new StringBuilder("Tamanho (bytes)\tCaminho\tArquivo\tExtensão");
+            //sb = new StringBuilder("Tamanho (bytes)\tCaminho\tArquivo\tExtensão");
+            //zip = new StringBuilder("Tamanho (bytes)\tCaminho\tArquivo\tExtensão");
+            //System.IO.File.WriteAllText(baseDir + @"lista_arquivos.txt", "Tamanho (bytes)\tCaminho\tArquivo\tExtensão");
+            lista.Add("Tamanho (bytes)\tCaminho\tArquivo\tExtensão");
 
-			Stopwatch stopWatch = new Stopwatch();
+            Stopwatch stopWatch = new Stopwatch();
 			stopWatch.Start();
 
 			CreateList(sPath);
@@ -209,23 +267,34 @@ namespace ListaArquivos {
 			TimeSpan ts = stopWatch.Elapsed;
 			string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}", ts.Hours, ts.Minutes, ts.Seconds, ts.Milliseconds);
 			Console.WriteLine($"CreateList RunTime {elapsedTime}");
-			//System.Windows.Forms.MessageBox.Show(elapsedTime, "CreateList RunTime", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            //System.Windows.Forms.MessageBox.Show(elapsedTime, "CreateList RunTime", MessageBoxButtons.OK, MessageBoxIcon.Information);
 
-			//System.IO.File.WriteAllText($"{baseDir}lista_arquivos.txt", sb.ToString());
-			//if (System.IO.File.Exists($"{baseDir}lista_arquivos.zip")) System.IO.File.Delete($"{baseDir}lista_arquivos.zip");
+            //System.IO.File.WriteAllText($"{baseDir}lista_arquivos.txt", sb.ToString());
 
-			using (var zipToOpen = new System.IO.FileStream($"{baseDir}lista_arquivos.zip", System.IO.FileMode.Create)) {
+            System.IO.File.AppendAllLines($"{baseDir}lista_arquivos.txt", lista);
+            lista.Clear();
+            lista.TrimExcess();
+
+            using (var zipToOpen = new System.IO.FileStream($"{baseDir}lista_arquivos.zip", System.IO.FileMode.Create)) {
 
 				using(var archive = new ZipArchive(zipToOpen, ZipArchiveMode.Update)) {
 
-					var listEntry = archive.CreateEntry("lista_arquivos.txt");
-
-					using(var writer = new System.IO.StreamWriter(listEntry.Open())) {
-						writer.WriteLine(sb);
-					}
+					archive.CreateEntryFromFile($"{baseDir}lista_arquivos.txt", "lista_arquivos.txt");
 				}
 			}
 
+            //  using(var zipToOpen = new System.IO.FileStream($"{baseDir}lista_arquivos.zip", System.IO.FileMode.Create)) {
+
+			//	using(var archive = new ZipArchive(zipToOpen, ZipArchiveMode.Update)) {
+
+			//		var listEntry = archive.CreateEntry("lista_arquivos.txt");
+
+			//		using(var writer = new System.IO.StreamWriter(listEntry.Open())) {
+			//			writer.WriteLine(sb);
+			//		}
+			//	}
+			//}
+            
 			//System.IO.File.WriteAllText(baseDir + @"lista_arquivos_zip.txt", zip.ToString());
 
 			button.IsEnabled = true;
